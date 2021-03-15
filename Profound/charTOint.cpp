413c9251b90303e0cd592479b1c8bbfe5820b7c9
Ahire Hitesh <hbahire2014@gmail.com>
2021-03-15 20:13:24 +0530

    add coversion

--- Profound/charTOint.cpp
+++ Profound/charTOint.cpp
@@ -5,11 +5,13 @@ int main(){
     char str = '5';
     int num1 = str-'0';
     int num2 = str - 48;
+    int num3 = ((int)str) - ((int)'0');
     
-    printf("Desimal value of char : %d", '0');
-    cout<<endl<<"String data str       : "<<str;
-    cout<<endl<<"str - 0 = num1        : "<<++num1;
-    cout<<endl<<"str - 48 = num2       : "<<++num2;
+    printf("Desimal value of char          	     : %d", str);
+    cout<<endl<<"String data str                      : "<<str;
+    cout<<endl<<"str - 0 = ++num1                     : "<<++num1;
+    cout<<endl<<"str - 48 = ++num2                    : "<<++num2;
+    cout<<endl<<"((int)str) - ((int)'0') = num3       : "<<++num3;
     
     return 0;
 }
